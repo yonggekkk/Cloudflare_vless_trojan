8352ba7248b7f3062abb288f2993ee8cdd4c4933
behbodrezai <138313966+behbodrezai@users.noreply.github.com>
2024-05-11 23:09:03 +0330

    workgkkkk

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '77a571fb-4fd2-4b37-8596-1b7d9728bb5c';
+let userID = '088f231b-e466-436b-a4a6-3f4b8d7f2fb8';
 
 const proxyIPs = ["cdn.xn--b6gac.eu.org"]; //workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
 const cn_hostnames = [''];
