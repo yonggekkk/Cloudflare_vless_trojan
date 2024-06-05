1370e0c071d1ce1abe9126c4dc5c3826074fee8f
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-06-05 12:41:57 +0800

    Update _worker.js

--- Trojan_workers_pages/_worker.js
+++ Trojan_workers_pages/_worker.js
@@ -23,7 +23,7 @@ const worker_default = {
       if (!upgradeHeader || upgradeHeader !== "websocket") {
         const url = new URL(request.url);
         switch (url.pathname) {
-          case "/cf":
+          case "/":
             return new Response(JSON.stringify(request.cf, null, 4), {
               status: 200,
               headers: {
