a1f3050bc08f5596bc8af61d36c039f61a8dc9ff
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-07-25 13:10:49 +0800

    Update _worker.js

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -851,7 +851,7 @@ ${displayHtml}
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Cloudflare-workers/pages-vless代理脚本 V24.7.21</h1>
+            <h1>Cloudflare-workers/pages-vless代理脚本 V24.7.25</h1>
 	    <hr>
             <p>${noteshow}</p>
             <hr>
@@ -924,7 +924,7 @@ ${displayHtml}
 			<br>
 			<h3>3：聚合通用、Clash-meta、Sing-box订阅链接如下：</h3>
 			<hr>
-			<p>注意：<br>1、默认每个订阅链接包含TLS+非TLS共13个端口节点<br>2、当前workers域名作为订阅链接，需通过代理进行订阅更新<br>3、需要开启分片功能使TLS节点可用，否则仅非TLS节点可用</p>
+			<p>注意：<br>1、默认每个订阅链接包含TLS+非TLS共13个端口节点<br>2、当前workers域名作为订阅链接，需通过代理进行订阅更新<br>3、sing-box订阅已集成分片功能，并不保证每个客户端可用，否则仅非TLS节点可用</p>
 			<hr>
 			<table class="table">
 					<thead>
@@ -983,7 +983,7 @@ ${displayHtml}
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Cloudflare-workers/pages-vless代理脚本 V24.7.21</h1>
+            <h1>Cloudflare-workers/pages-vless代理脚本 V24.7.25</h1>
 			<hr>
             <p>${noteshow}</p>
             <hr>
@@ -1592,6 +1592,8 @@ return `{
 		  "uuid": "${userID}"
 		},
 		{
+		  "tcp_fast_open": true,
+		  "udp_fragment": true,      
 		  "server": "${IP8}",
 		  "server_port": ${PT8},
 		  "tag": "CF_V8_${IP8}_${PT8}",
@@ -1618,6 +1620,8 @@ return `{
 		  "uuid": "${userID}"
 		},
 		{
+		  "tcp_fast_open": true,
+		  "udp_fragment": true, 
 		  "server": "${IP9}",
 		  "server_port": ${PT9},
 		  "tag": "CF_V9_${IP9}_${PT9}",
@@ -1644,6 +1648,8 @@ return `{
 		  "uuid": "${userID}"
 		},
 		{
+		  "tcp_fast_open": true,
+		  "udp_fragment": true, 
 		  "server": "${IP10}",
 		  "server_port": ${PT10},
 		  "tag": "CF_V10_${IP10}_${PT10}",
@@ -1670,6 +1676,8 @@ return `{
 		  "uuid": "${userID}"
 		},
 		{
+		  "tcp_fast_open": true,
+		  "udp_fragment": true, 
 		  "server": "${IP11}",
 		  "server_port": ${PT11},
 		  "tag": "CF_V11_${IP11}_${PT11}",
@@ -1696,6 +1704,8 @@ return `{
 		  "uuid": "${userID}"
 		},
 		{
+		  "tcp_fast_open": true,
+		  "udp_fragment": true, 
 		  "server": "${IP12}",
 		  "server_port": ${PT12},
 		  "tag": "CF_V12_${IP12}_${PT12}",
@@ -1722,6 +1732,8 @@ return `{
 		  "uuid": "${userID}"
 		},
 		{
+		  "tcp_fast_open": true,
+		  "udp_fragment": true, 
 		  "server": "${IP13}",
 		  "server_port": ${PT13},
 		  "tag": "CF_V13_${IP13}_${PT13}",
