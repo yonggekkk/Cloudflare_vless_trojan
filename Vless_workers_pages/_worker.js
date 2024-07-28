b27c75e6384fe339ccb6bbb8b0e193af221df182
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-07-28 22:29:52 +0800

    Update _worker.js

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -6,7 +6,7 @@ import { connect } from "cloudflare:sockets";
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = "77a571fb-4fd2-4b37-8596-1b7d9728bb5c";
 
-const proxyIPs = ["proxy.xxxxxxxx.tk"]; //ts.hpc.tw workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
+const proxyIPs = ["proxy.xxxxxxxx.tk"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 const cn_hostnames = [''];
 let CDNIP = 'www.visa.com.sg'
 // http_ip
