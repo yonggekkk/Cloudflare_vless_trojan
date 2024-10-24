cb4d48f92b96608b132e4a2a0f1832b2c8a97c8e
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-10-24 09:58:07 +0800

    Create cf.sh

---
+++ cf/cf.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+export LANG=en_US.UTF-8
+case "$(uname -m)" in
+	x86_64 | x64 | amd64 )
+	cpu=amd64
+	;;
+	i386 | i686 )
+        cpu=386
+	;;
+	armv8 | armv8l | arm64 | aarch64 )
+        cpu=arm64
+	;;
+	armv7l )
+        cpu=arm
+	;;
+        mips64le )
+        cpu=mips64le
+	;;
+        mips64 )
+        cpu=mips64
+	;;
+        mips )
+        cpu=mipsle
+	;;
+        mipsle )
+        cpu=mipsle
+	;;
+	* )
+	echo "当前架构为$(uname -m)，暂不支持"
+	exit
+	;;
+esac
+
+result(){
+awk -F ',' '$2 ~ /BGI|YCC|YVR|YWG|YHZ|YOW|YYZ|YUL|YXE|STI|SDQ|GUA|KIN|GDL|MEX|QRO|SJU|MGM|ANC|PHX|LAX|SMF|SAN|SFO|SJC|DEN|JAX|MIA|TLH|TPA|ATL|HNL|ORD|IND|BGR|BOS|DTW|MSP|MCI|STL|OMA|LAS|EWR|ABQ|BUF|CLT|RDU|CLE|CMH|OKC|PDX|PHL|PIT|FSD|MEM|BNA|AUS|DFW|IAH|MFE|SAT|SLC|IAD|ORF|RIC|SEA/ {print $0}' /root/$ip.csv | sort -t ',' -k5,5n | head -n 3 > /root/US-$ip.csv
+awk -F ',' '$2 ~ /CGP|DAC|JSR|PBH|BWN|PNH|GUM|HKG|AMD|BLR|BBI|IXC|MAA|HYD|CNN|KNU|COK|CCU|BOM|NAG|DEL|PAT|DPS|CGK|JOG|FUK|OKA|KIX|NRT|ALA|NQZ|ICN|VTE|MFM|JHB|KUL|KCH|MLE|ULN|MDL|RGN|KTM|ISB|KHI|LHE|CGY|CEB|MNL|CRK|KJA|SVX|SIN|CMB|KHH|TPE|BKK|CNX|URT|TAS|DAD|HAN|SGN/ {print $0}' /root/$ip.csv | sort -t ',' -k5,5n | head -n 3 > /root/AS-$ip.csv
+awk -F ',' '$2 ~ /TIA|VIE|MSQ|BRU|SOF|ZAG|LCA|PRG|CPH|TLL|HEL|BOD|LYS|MRS|CDG|TBS|TXL|DUS|FRA|HAM|MUC|STR|ATH|SKG|BUD|KEF|ORK|DUB|MXP|PMO|FCO|RIX|VNO|LUX|KIV|AMS|SKP|OSL|WAW|LIS|OTP|DME|LED|KLD|BEG|BTS|BCN|MAD|GOT|ARN|GVA|ZRH|IST|ADB|KBP|EDI|LHR|MAN/ {print $0}' /root/$ip.csv | sort -t ',' -k5,5n | head -n 3 > /root/EU-$ip.csv
+}
+
+#if timeout 3 ping -c 2 google.com &> /dev/null; then
+#echo "当前网络已开代理，为确保准确性，请关闭代理"
+#else
+#echo "当前网络已关闭代理，继续进行……"
+#fi
+
+if timeout 3 ping -c 2 2400:3200::1 &> /dev/null; then
+echo "当前网络支持IPV4+IPV6"
+else
+echo "当前网络仅支持IPV4"
+fi
+rm -rf 6.csv 4.csv
+echo "请选择优选类型"
+echo "1、仅IPV4优选"
+echo "2、仅IPV6优选"
+echo "3、IPV4+IPV6优选"
+read -p "请选择【1-3】:" menu
+curl -L -o cf -# --retry 2 --insecure https://raw.githubusercontent.com/yonggekkk/Cloudflare_vless_trojan/main/cf/$cpu
+curl -s -o locations.json https://raw.githubusercontent.com/yonggekkk/Cloudflare_vless_trojan/main/cf/locations.json
+curl -s -o ips-v4.txt https://raw.githubusercontent.com/yonggekkk/Cloudflare_vless_trojan/main/cf/ips-v4.txt
+curl -s -o ips-v6.txt https://raw.githubusercontent.com/yonggekkk/Cloudflare_vless_trojan/main/cf/ips-v6.txt
+chmod +x /root/cf
+echo "接下来进行优选……此过程可能需要30秒……" && sleep 2
+if [ "$menu" = "1" ]; then
+ip=4
+./cf -ips $ip -outfile $ip.csv
+result
+elif [ "$menu" = "2" ]; then
+ip=6
+./cf -ips $ip -outfile $ip.csv
+result
+elif [ "$menu" = "3" ]; then
+ip=4
+./cf -ips $ip -outfile $ip.csv
+result
+ip=6
+./cf -ips $ip -outfile $ip.csv
+result
+fi
+clear
+if [ -e 4.csv ]; then
+echo "IPV4最佳可用节点如下（取前三名）："
+echo "美国IPV4优选结果："
+cat US-4.csv
+echo
+echo "亚洲IPV4优选结果："
+cat AS-4.csv
+echo
+echo "欧洲IPV4优选结果："
+cat EU-4.csv
+fi
+if [ -e 6.csv ]; then
+echo "IPV6最佳可用节点如下（取前三名）："
+echo "美国IPV6优选结果："
+cat US-6.csv
+echo
+echo "亚洲IPV6优选结果："
+cat AS-6.csv
+echo
+echo "欧洲IPV6优选结果："
+cat EU-6.csv
+fi
+if [ ! -e 4.csv ] && [ ! -e 6.csv ]; then
+echo "运行出错，请检查网络依赖环境"
+fi
