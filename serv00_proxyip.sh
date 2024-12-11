8d5e9056ff497f46bc22c61243aad25f82f14e62
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-11 10:46:59 +0800

    Update serv00_proxyip.sh

--- serv00_proxyip.sh
+++ serv00_proxyip.sh
@@ -159,6 +159,7 @@ argo_configure() {
   while true; do
     yellow "Argo临时隧道 (无需域名，推荐)"
     yellow "Argo固定隧道 (需要域名，需要CF设置提取Token)"
+    echo -e "${red}注意：${purple}Argo固定隧道使用token时，需要在cloudflare后台设置隧道端口，该端口必须与vmess+ws的tcp端口一致)${re}"
     reading "输入 g 表示使用Argo固定隧道 ；回车跳过 表示使用Argo临时隧道 【g/回车】: " argo_choice
     if [[ "$argo_choice" != "g" && "$argo_choice" != "G" && -n "$argo_choice" ]]; then
         red "无效的选择，请输入 g 或回车"
@@ -169,7 +170,6 @@ argo_configure() {
         green "你的argo固定隧道域名为: $ARGO_DOMAIN"
         reading "请输入argo固定隧道密钥（Json或Token）: " ARGO_AUTH
         green "你的argo固定隧道密钥为: $ARGO_AUTH"
-        echo -e "${red}注意：${purple}使用token，需要在cloudflare后台设置隧道端口和vmess+ws的tcp端口一致${re}"
     else
         green "使用Argo临时隧道"
     fi
