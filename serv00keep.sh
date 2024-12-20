ce5c9122ad56078b02b17ab4c6505d5cc6452192
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-20 09:14:24 +0800

    Update serv00keep.sh

--- serv00keep.sh
+++ serv00keep.sh
@@ -135,9 +135,8 @@ for entry in "${FILE_INFO[@]}"; do
 done
 wait
 
-output=$(./"$(basename ${FILE_MAP[web]})" generate reality-keypair)
-private_key=$(echo "${output}" | awk '/PrivateKey:/ {print $2}')
-public_key=$(echo "${output}" | awk '/PublicKey:/ {print $2}')
+private_key=$(<private_key.txt)
+public_key=$(<public_key.txt)
 
 openssl ecparam -genkey -name prime256v1 -out "private.key"
 openssl req -new -x509 -days 3650 -key "private.key" -out "cert.pem" -subj "/CN=$USERNAME.serv00.net"
