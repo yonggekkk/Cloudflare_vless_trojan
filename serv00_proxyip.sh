55a5de02b8bbc92a9d7e18565b26bc526ad948d1
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2024-12-24 13:56:11 +0800

    Update serv00_proxyip.sh

--- serv00_proxyip.sh
+++ serv00_proxyip.sh
@@ -137,6 +137,9 @@ reading "\n清理所有进程并清空所有安装内容，将退出ssh连接，
   case "$choice" in
     [Yy]) 
     ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 2>/dev/null
+    crontab -l | grep -v "serv00keep" >rmcron
+    crontab rmcron >/dev/null 2>&1
+    rm rmcron
     find ~ -type f -exec chmod 644 {} \; 2>/dev/null
     find ~ -type d -exec chmod 755 {} \; 2>/dev/null
     find ~ -type f -exec rm -f {} \; 2>/dev/null
