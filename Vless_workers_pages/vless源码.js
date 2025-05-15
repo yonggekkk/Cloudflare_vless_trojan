02f78c892645e1f108e88858ca8b4d68aa89813a
Yongjiu0801 <153344245+Yongjiu0801@users.noreply.github.com>
2025-05-15 19:29:45 +0800

    Update vless源码.js

--- Vless_workers_pages/vless源码.js
+++ Vless_workers_pages/vless源码.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "450b892f-5b83-4726-a727-f0893402b09d";
 
 const proxyIPs = ["xjp.ygkkk.dpdns.org:60360"];
 const cn_hostnames = [''];
