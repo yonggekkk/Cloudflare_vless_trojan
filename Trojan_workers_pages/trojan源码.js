d6dd12d6783460fb6f1f816df517b0c2530dfe52
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-05-21 17:32:59 +0800

    Update trojan源码.js

--- Trojan_workers_pages/trojan源码.js
+++ Trojan_workers_pages/trojan源码.js
@@ -2,7 +2,7 @@
 import { connect } from "cloudflare:sockets";
  
 let Pswd = "trojan";
-const proxyIPs = ["xjp.ygkkk.dpdns.org:60360"];
+const proxyIPs = ["ts.hpc.tw"];
 let cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
